b68586cfdff78316570c6b2ad4ee296d53fc120c
utkarsh032 <utkarshhraj525@gmail.com>
2023-04-23 11:13:30 +0530

    work left for fill data to contentfull to make api key

---
+++ src/Contentful.js
@@ -0,0 +1,12 @@
+// const contentful = require("contentful");
+
+// export default contentful.createClient({
+//   space: process.env.REACT_APP_API_SPACE,
+//   accessToken: process.env.REACT_APP_ACCESS_TOKEN
+// });
+
+// here need to entry data to contentful : https://app.contentful.com/spaces/zbr9ow5gfybg/content_types?order.fieldId=name&order.direction=ascending
+
+// to make api and
+
+// npm install contentfull
